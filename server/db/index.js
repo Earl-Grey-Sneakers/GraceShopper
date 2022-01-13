8e476137868806731d812bbb17f64e235c4b29c2
Danny Pacheco <pacheco4@buffalo.edu>
2022-01-13 18:59:59 -0500

    fixed adding to cart bugs and having backend errors

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,7 @@
 //this is the access point for all things database related!
 
 const db = require('./db');
+const Sequelize = require('sequelize');
 const Style = require('./models/Style');
 const OrderPayment = require('./models/OrderPayment');
 const Order = require('./models/Order');
@@ -10,6 +11,32 @@ const User = require('./models/User');
 //Associations
 Order.belongsTo(User);
 User.hasMany(Order);
+const orderItems = db.define('orderItems', {
+  styleId: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: Style,
+      key: 'id'
+    }
+  },
+  orderId: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: Order, // 'Actors' would also work
+      key: 'id'
+    }
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0,
+    validate: {
+      min: 0
+    }
+  },
+  totalPrice: {
+    type: Sequelize.FLOAT
+  }
+});
 Style.belongsToMany(Order, { through: 'orderItems' });
 Order.belongsToMany(Style, { through: 'orderItems' });
 Order.hasOne(OrderPayment);
