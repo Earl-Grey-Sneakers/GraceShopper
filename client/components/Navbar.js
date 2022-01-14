0d807328d8032f1d43c007c22d3d6e78f5f455d2
Smit Patel <smitpatel.h@gmail.com>
2022-01-13 21:26:50 -0600

    All styles page refactored

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,78 +1,34 @@
 import React from 'react';
-import { connect, useSelector } from 'react-redux';
+import { connect, useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { logout } from '../store';
 
 const NavBar = () => {
-  const { username } = useSelector((state) => {
+  const { username, id } = useSelector((state) => {
     return state.auth;
   });
+  const dispatch = useDispatch();
   return (
-    // <nav class="autohide navbar navbar-expand-lg navbar-dark bg-primary">
-    //   <div class="container-fluid">
-    //     <a class="navbar-brand" href="#">
-    //       Brand
-    //     </a>
-    //     <button
-    //       class="navbar-toggler"
-    //       type="button"
-    //       data-bs-toggle="collapse"
-    //       data-bs-target="#main_nav"
-    //     >
-    //       <span class="navbar-toggler-icon"></span>
-    //     </button>
-    //     <div class="collapse navbar-collapse" id="main_nav">
-    //       <ul class="navbar-nav ms-auto">
-    //         <li class="nav-item">
-    //           <a class="nav-link" href="#">
-    //             Menu item
-    //           </a>
-    //         </li>
-    //         <li class="nav-item">
-    //           <a class="nav-link" href="#">
-    //             Menu item
-    //           </a>
-    //         </li>
-    //         <li class="nav-item">
-    //           <a class="nav-link" href="#">
-    //             Menu item
-    //           </a>
-    //         </li>
-    //       </ul>
-    //     </div>
-    //   </div>
-    //   <script type="text/javascript">
-    //     {document.addEventListener('DOMContentLoaded', function () {
-    //       el_autohide = document.querySelector('.autohide');
-    //       // add padding-top to bady (if necessary)
-    //       navbar_height = document.querySelector('.navbar').offsetHeight;
-    //       document.body.style.paddingTop = navbar_height + 'px';
-    //       if (el_autohide) {
-    //         var last_scroll_top = 0;
-    //         window.addEventListener('scroll', function () {
-    //           let scroll_top = window.scrollY;
-    //           if (scroll_top < last_scroll_top) {
-    //             el_autohide.classList.remove('scrolled-down');
-    //             el_autohide.classList.add('scrolled-up');
-    //           } else {
-    //             el_autohide.classList.remove('scrolled-up');
-    //             el_autohide.classList.add('scrolled-down');
-    //           }
-    //           last_scroll_top = scroll_top;
-    //         });
-    //         // window.addEventListener
-    //       }
-    //       // if
-    //     })}
-    //   </script>
-    // </nav>
-
     <header className="">
-      <Link to='/styles' ><h2 className="logo">EGSH</h2></Link>
+      <Link to="/styles">
+        <h2 className="logo">EGSH</h2>
+      </Link>
       <ul>
         <li>{username ? <div>Welcome, {username}</div> : <div>Welcome, Guest</div>}</li>
         <li>
-          <Link to='/cart' ><i className="gg-shopping-bag"></i></Link>
+          <Link to="/cart">
+            <i className="gg-shopping-bag"></i>
+          </Link>
         </li>
+        {id ? (
+          <li onClick={() => dispatch(logout())} className="logout">
+            Logout
+          </li>
+        ) : (
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        )}
       </ul>
       <script type="text/javascript">
         {window.addEventListener('scroll', function () {
