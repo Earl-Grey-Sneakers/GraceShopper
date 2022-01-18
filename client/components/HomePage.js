482754041cea734c47836d53409ca58e8cadfe58
Danny Pacheco <pacheco4@buffalo.edu>
2022-01-18 00:17:53 -0500

    homepage shows random shoes and links to their single view page

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchStyles } from '../store/styles';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
+    const styles =
+    useSelector((state) => {
+      return state.styles;
+    }) || [];
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(fetchStyles())
+    }, [])
+
+    const randomStyles=[]
+    let style={}
+    if (styles.length>0) {
+        for(let i=0; i<4; i++){
+            let num = Math.floor(Math.random()*styles.length)
+            randomStyles.push(styles[num])
+            styles.splice(num,1)
+        }
+        let num = Math.floor(Math.random()*styles.length)
+        style= styles[num]
+    }
+
     return (
         <div className="divBelowNavbar">
             <div className="featuredShoe">
                 <h3 className="featured-shoe-text">Featured Shoe</h3>
-                <img id="featured-shoe" src='https://images.stockx.com/images/Air-Jordan-6-Retro-Carmine-2020-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&dpr=2&trim=color&updated_at=1612286105'/>
+                <Link to={`/styles/${style.shoeName}`} >
+                <img id="featured-shoe" src={style.imageUrl}/>
+                </Link>
                 <hr />
             </div>
             <div className="brands">
@@ -15,14 +43,19 @@ const HomePage = () => {
             </div>
             <hr />
             <div className="random-shoes">
-                <img  className="random-sneaker" src='https://images.stockx.com/images/Adidas-Yeezy-Boost-350-V2-Core-Black-Red-2017-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&dpr=2&trim=color&updated_at=1606320792'/>
-                <img className="random-sneaker" src='https://images.stockx.com/images/Air-Jordan-1-Retro-Bred-2016-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&dpr=2&trim=color&updated_at=1606318705'/>
-                <img className="random-sneaker" src='https://images.stockx.com/images/Nike-Dunk-Low-UNC-2021-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&dpr=2&trim=color&updated_at=1624468252'/>
-                <img className="random-sneaker" src='https://images.stockx.com/images/New-Balance-550-Aime-Leon-Dore-White-Green-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&dpr=2&trim=color&updated_at=1606765288'/>
+                {randomStyles.map((style,idx) => (
+                    <Link key={idx} to={`/styles/${style.shoeName}`} >
+                    <img className="random-sneaker" src={style.imageUrl}/>
+                    </Link>
+                ))}
             </div>
             <div className="onFeet">
-                <img className="on-feet-pic" src='https://cms-cdn.thesolesupplier.co.uk/2020/12/bred5_w1160.jpg'/>
-                <img className="on-feet-pic" src='https://www.allaboutanthony.com/wp-content/uploads/2019/08/Air-Jordan-1-Low-Koston-Nike-SB-WDYWT-On-Feet-Blue-Laces.jpg'/>
+                <Link to={`/styles/Breds`} >
+                    <img className="on-feet-pic" src='https://cms-cdn.thesolesupplier.co.uk/2020/12/bred5_w1160.jpg'/>
+                </Link>
+                <Link to={`/styles/UNC Dunk`} >
+                    <img className="on-feet-pic" src='https://www.allaboutanthony.com/wp-content/uploads/2019/08/Air-Jordan-1-Low-Koston-Nike-SB-WDYWT-On-Feet-Blue-Laces.jpg'/>
+                </Link>
             </div>
         </div>
     )
