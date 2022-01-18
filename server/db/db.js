94093b12ea094c9ef10eaf8322467eb0d8ef9b88
Gulam <g.u.islam30@gmail.com>
2022-01-18 16:44:47 -0500

    Changed Database to DATABASE_URL

--- server/db/db.js
+++ server/db/db.js
@@ -12,7 +12,7 @@ if(process.env.LOGGING === 'true'){
 }
 
 //https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
-if(process.env.DATABASE){
+if(process.env.DATABASE_URL){
   config.dialectOptions = {
     ssl: {
       rejectUnauthorized: false
@@ -21,5 +21,5 @@ if(process.env.DATABASE){
 }
 
 const db = new Sequelize(
-  process.env.DATABASE || `postgres://localhost:5432/${databaseName}`, config)
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`, config)
 module.exports = db
