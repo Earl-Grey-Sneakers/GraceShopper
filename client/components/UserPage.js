7995c3c3fb92a3b0d8f3afdfae62bb2a7de6a02d
Gulam <g.u.islam30@gmail.com>
2022-01-19 01:26:20 -0500

    Created Skeleton for Most Recent Order

--- client/components/UserPage.js
+++ client/components/UserPage.js
@@ -14,10 +14,46 @@ const UserPage = () => {
 
     return (
         <div className="divBelowNavbar">
-            <h1>Account Overview</h1>
-            <h3>Welcome to your Account Page, {username} </h3>
-            <h3>Email Address: {email}</h3>
-            {/* <h4>Account Number: {id}</h4> */}
+            <div className="Account Infor">
+                <h1>Account Overview</h1>
+                <h3>Welcome to your Account Page, {username} </h3>
+                <h3>Email Address: {email}</h3>
+            </div>
+            <div className="latestOrder">
+                <table>
+                        Most Recent Order
+                    <td>
+                        <tr>
+                            <tr>Order #:</tr>
+                            <td>24987234</td>
+                        </tr>
+                        <tr>
+                            <tr>Order Size:</tr>
+                            <td>2</td>
+                        </tr>
+                        <tr>
+                            <tr>Total Cost:</tr>
+                            <td>190</td>
+                        </tr>
+                        <tr>
+                            Billing Adress:
+                            <td>
+                                123 STREET
+                                FAKE CITY
+                                STATE
+                                ZIPCODE
+                            </td>
+                        </tr>
+                        <tr>
+                            Payment Information:
+                            <td>
+                            Card Ending in XXXX
+                            </td>
+                        </tr>
+                    </td>
+                </table>
+            </div>
+           
         </div>
         
     )
