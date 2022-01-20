89d18d98998048512534a44e0263c9599b8fc042
Smit Patel <smitp520.sp@gmail.com>
2022-01-19 19:53:42 -0600

    css added to cart page

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -23,55 +23,82 @@ const Cart = () => {
   }, [userId]);
 
   return (
-    <div className="divBelowNavbar">
-      <h1 className="my-cart-title">My Cart</h1>
+    <div>
       {cartItems.length != 0 ? (
         <div>
-          <h3>
-            Your Total: {'$'}
-            {cart.orderTotal}
-          </h3>
-          <div className="wrapper">
-            {cartItems.map((item) => (
-              <div key={item.id} className="card">
-                <div>
-                  <h3>{item['shoeName']}</h3>
-                  <h5>
-                    {'$'}
-                    {item['price']}
-                  </h5>
-                  <Link to={`/styles/${item.shoeName}`}>
-                    <img className="shoe-img" src={item['imageUrl']} />
-                  </Link>
-                  <h5>Size:{item.size}</h5>
+          <h3>Bag</h3>
+          <div className="row item">
+            <div className="column">
+              {cartItems.map((item) => (
+                <div key={item.id} className="row cartItem">
+                  <div className="column ">
+                    <Link to={`/styles/${item.shoeName}`}>
+                      <img className="shoe-img" src={item['imageUrl']} />
+                    </Link>
+                  </div>
+                  <div className="column">
+                    <h3>{item['shoeName']}</h3>
+                    <h5>
+                      {'$'}
+                      {item['price']}
+                    </h5>
+
+                    <h5>Size:{item.size}</h5>
+
+                    <button
+                      onClick={() => {
+                        if (item.orderItems.quantity > 1) {
+                          dispatch(updateQuantities(cart.id, UUID, userId, item.id, 'dec'));
+                        } else {
+                          dispatch(removeCartItem(cart.id, item.id, userId, UUID));
+                        }
+                      }}
+                    >
+                      -
+                    </button>
+                    <span>Qty: {item.orderItems.quantity}</span>
+
+                    <button
+                      onClick={() =>
+                        dispatch(updateQuantities(cart.id, UUID, userId, item.id, 'inc'))
+                      }
+                    >
+                      +
+                    </button>
+                    <div>
+                      <i
+                        className="bi bi-trash"
+                        onClick={() => dispatch(removeCartItem(cart.id, item.id, userId, UUID))}
+                      ></i>
+                    </div>
+                  </div>
                 </div>
-                <button
-                  className="button-24"
-                  onClick={() => dispatch(removeCartItem(cart.id, item.id, userId, UUID))}
-                >
-                  Remove item
-                </button>
-                <button
-                  onClick={() => {
-                    if (item.orderItems.quantity > 1) {
-                      dispatch(updateQuantities(cart.id, UUID, userId, item.id, 'dec'));
-                    } else {
-                      dispatch(removeCartItem(cart.id, item.id, userId, UUID));
-                    }
-                  }}
-                >
-                  -
-                </button>
-                <span>Qty: {item.orderItems.quantity}</span>
-                <button
-                  onClick={() => dispatch(updateQuantities(cart.id, UUID, userId, item.id, 'inc'))}
-                >
-                  +
-                </button>
+              ))}
+            </div>
+            <div className="column summary">
+              <h3>Summary</h3>
+              <div className="row">
+                <div className="column">Subtotal </div>
+                <div className="column">
+                  {'$'} {cart.orderTotal}.00
+                </div>
               </div>
-            ))}
-          </div>
-          {auth.id ? (
+              <div className="row">
+                <div className="column">Estimated Shipping & Handling</div>
+                <div className="column">$1200.00</div>
+              </div>
+              <div className="row">
+                <div className="column">Estimated Tax</div>
+                <div className="column">10%</div>
+              </div>
+              <div className="row total">
+                <div className="column">Total</div>
+                <div className="column">
+                  {'$'} {cart.orderTotal + 1200 + (cart.orderTotal + 1200) * 0.1}.00
+                </div>
+              </div>
+            </div>
+            {auth.id ? (
               <button
                 className="button-30"
                 id="checkout-btn"
@@ -83,16 +110,14 @@ const Cart = () => {
               >
                 Checkout
               </button>
-          ) : (
-            <Link to='signup'>
-            <button
-                className="button-30"
-                id="checkout-btn"
-              >
-                Sign up
-              </button>
-            </Link>
-          )}
+            ) : (
+              <Link to="signup">
+                <button className="button-30" id="checkout-btn">
+                  Sign up
+                </button>
+              </Link>
+            )}
+          </div>
         </div>
       ) : (
         <div className="wrapper extendDiv">
